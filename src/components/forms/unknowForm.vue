998eaa0e5c47a69e9b802657c8072dcad0f0595d
Eric Chiu <eric840906@hotmail.com.tw>
2020-10-10 16:27:19 +0800

    added form

--- src/components/forms/unknowForm.vue
+++ src/components/forms/unknowForm.vue
@@ -2,9 +2,61 @@
   <div class="back">
     <div class="formContainer col-md-6">
       <div>
-        <h3>是否了解自己的基礎代謝率?</h3>
-        <router-link to="/knowForm">是</router-link> <router-link to="/unknowForm">否</router-link>
+        <form action="" @submit.prevent="calcBmr">
+          <p>性別</p>
+          <div class="genderSec">
+            <div>
+              <input type="radio" name="gender" id="male" value="男" required>
+              <label for="male">男</label>
+            </div>
+            <div>
+              <input type="radio" name="gender" id="female" value="女">
+              <label for="female">女</label>
+            </div>
+          </div>
+          <label for="age">年齡</label>
+          <input type="number" name="age" placeholder="輸入年齡" required style="display: block;">
+          <label for="height">身高</label>
+          <input type="number" name="height" placeholder="輸入身高" required style="display: block;">
+          <label for="weight">體重</label>
+          <input type="number" name="weight" placeholder="輸入體重" required style="display: block;">
+          <input type="submit" value="送出" style="margin-top: 10px">
+        </form>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+    calcBmr (e) {
+      const vm = this
+      console.log(e)
+      const gender = e.target[0].checked ? 'male' : 'female'
+      const age = e.target[2].valueAsNumber
+      const height = e.target[3].valueAsNumber
+      const weight = e.target[4].valueAsNumber
+      console.log(gender, age, height, weight)
+      const BMR = gender === 'male' ? Math.round(66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age)) : Math.round(665.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age))
+      console.log(BMR)
+      localStorage.setItem('BMR', BMR)
+      vm.$router.push('/tdeeForm')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .genderSec{
+    display: flex;
+    div{
+      &:first-child{
+        margin-right: 18px;
+      }
+    }
+  }
+</style>
